741fa0987a463dd2e5859849d1c50387104b8c54
Ubuntu <ubuntu@ip-172-31-18-1.us-west-1.compute.internal>
2023-03-20 10:27:19 +0000

    feat: add a Makefile and a script o run a local node

---
+++ local_node.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+KEYS[0]="dev0"
+KEYS[1]="dev1"
+KEYS[2]="dev2"
+CHAINID="xnet_03012023-1"
+MONIKER="localtestnet"
+# Remember to change to other types of keyring like 'file' in-case exposing to outside world,
+# otherwise your balance will be wiped quickly
+# The keyring test does not require private key to steal uCCs from you
+KEYRING="test"
+LOGLEVEL="info"
+# Set dedicated home directory for the xnetd instance
+HOMEDIR="$HOME/.xnet"
+# to trace evm
+#TRACE="--trace"
+TRACE=""
+
+# Path variables
+CONFIG=$HOMEDIR/config/config.toml
+APP_TOML=$HOMEDIR/config/app.toml
+GENESIS=$HOMEDIR/config/genesis.json
+TMP_GENESIS=$HOMEDIR/config/tmp_genesis.json
+
+# validate dependencies are installed
+command -v jq >/dev/null 2>&1 || {
+	echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"
+	exit 1
+}
+
+# used to exit on first error (any non-zero exit code)
+set -e
+
+# Reinstall daemon
+make install
+
+# User prompt if an existing local node configuration is found.
+if [ -d "$HOMEDIR" ]; then
+	printf "\nAn existing folder at '%s' was found. You can choose to delete this folder and start a new local node with new keys from genesis. When declined, the existing local node is started. \n" "$HOMEDIR"
+	echo "Overwrite the existing configuration and start a new local node? [y/n]"
+	read -r overwrite
+else
+	overwrite="Y"
+fi
+
+
+# Setup local node if overwrite is set to Yes, otherwise skip setup
+if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
+	# Remove the previous folder
+	rm -rf "$HOMEDIR"
+
+	# Set client config
+	xnetd config keyring-backend $KEYRING --home "$HOMEDIR"
+	xnetd config chain-id $CHAINID --home "$HOMEDIR"
+
+	# If keys exist they should be deleted
+	for KEY in "${KEYS[@]}"; do
+		xnetd keys add "$KEY" --keyring-backend $KEYRING --home "$HOMEDIR"
+	done
+
+	# Set moniker and chain-id for xnet (Moniker can be anything, chain-id must be an integer)
+	xnetd init $MONIKER -o --chain-id $CHAINID --home "$HOMEDIR"
+
+	# Change parameter uCC denominations to uCC
+	jq '.app_state["staking"]["params"]["bond_denom"]="uCC"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.app_state["crisis"]["constant_fee"]["denom"]="uCC"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uCC"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.app_state["mint"]["params"]["mint_denom"]="uCC"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+
+	# Set gas limit in genesis
+	jq '.consensus_params["block"]["max_gas"]="10000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+
+	if [[ $1 == "pending" ]]; then
+		if [[ "$OSTYPE" == "darwin"* ]]; then
+			sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' "$CONFIG"
+			sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' "$CONFIG"
+			sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' "$CONFIG"
+			sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' "$CONFIG"
+			sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' "$CONFIG"
+			sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' "$CONFIG"
+			sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' "$CONFIG"
+			sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' "$CONFIG"
+		else
+			sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' "$CONFIG"
+			sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' "$CONFIG"
+			sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' "$CONFIG"
+			sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' "$CONFIG"
+			sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' "$CONFIG"
+			sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' "$CONFIG"
+			sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' "$CONFIG"
+			sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' "$CONFIG"
+		fi
+	fi
+
+    # enable prometheus metrics
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' 's/prometheus = false/prometheus = true/' "$CONFIG"
+        sed -i '' 's/prometheus-retention-time = 0/prometheus-retention-time  = 1000000000000/g' "$APP_TOML"
+        sed -i '' 's/enabled = false/enabled = true/g' "$APP_TOML"
+    else
+        sed -i 's/prometheus = false/prometheus = true/' "$CONFIG"
+        sed -i 's/prometheus-retention-time  = "0"/prometheus-retention-time  = "1000000000000"/g' "$APP_TOML"
+        sed -i 's/enabled = false/enabled = true/g' "$APP_TOML"
+    fi
+	
+	# Change proposal periods to pass within a reasonable time for local testing
+	sed -i.bak 's/"max_deposit_period": "172800s"/"max_deposit_period": "30s"/g' "$HOMEDIR"/config/genesis.json
+	sed -i.bak 's/"voting_period": "172800s"/"voting_period": "30s"/g' "$HOMEDIR"/config/genesis.json
+
+	# set custom pruning settings
+	sed -i.bak 's/pruning = "default"/pruning = "custom"/g' "$APP_TOML"
+	sed -i.bak 's/pruning-keep-recent = "0"/pruning-keep-recent = "2"/g' "$APP_TOML"
+	sed -i.bak 's/pruning-interval = "0"/pruning-interval = "10"/g' "$APP_TOML"
+
+	# Allocate genesis accounts (cosmos formatted addresses)
+	for KEY in "${KEYS[@]}"; do
+		xnetd add-genesis-account "$KEY" 100000000000000000000000000uCC --keyring-backend $KEYRING --home "$HOMEDIR"
+	done
+
+	# bc is required to add these big numbers
+	total_supply=$(echo "${#KEYS[@]} * 100000000000000000000000000" | bc)
+	jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["denom"]="uCC"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+
+	# Sign genesis transaction
+	xnetd gentx "${KEYS[0]}" 1000000000000000000000uCC --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR"  --commission-max-change-rate=0.01 --commission-max-rate=1.0 --commission-rate=0.07
+	## In case you want to create multiple validators at genesis
+	## 1. Back to `xnetd keys add` step, init more keys
+	## 2. Back to `xnetd add-genesis-account` step, add balance for those
+	## 3. Clone this ~/.xnetd home directory into some others, let's say `~/.clonedxnetd`
+	## 4. Run `gentx` in each of those folders
+	## 5. Copy the `gentx-*` folders under `~/.clonedxnetd/config/gentx/` folders into the original `~/.xnetd/config/gentx`
+
+	# Collect genesis tx
+	xnetd collect-gentxs --home "$HOMEDIR"
+
+	# Run this to ensure everything worked and that the genesis file is setup correctly
+	xnetd validate-genesis --home "$HOMEDIR"
+
+	if [[ $1 == "pending" ]]; then
+		echo "pending mode is on, please wait for the first block committed."
+	fi
+fi
+
+# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+xnetd start "$TRACE" --log_level $LOGLEVEL --minimum-gas-prices=0.0001uCC --api.enable --home "$HOMEDIR"
